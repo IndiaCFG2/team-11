dd2689d2ad271205b2e187f98b05b0b3d5de7853
Abhilash <42943043+abhilash-thepioneer@users.noreply.github.com>
2020-08-08 21:21:21 +0530

    Update urls.py

--- KEF11/employee/urls.py
+++ KEF11/employee/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('map/',views.map,name='map'),
     path('list_schools/',views.list_schools,name='list_schools'),
     path('',views.login,name='login'),
+       path('signup/',views.register_employee,name='employee-create'),
+     path('login/',views.login_asview,name='login'),
+     path('logout/',views.logout_asview,name='logout'),
+     path('delete/<int:id1>',views.delete,name="delete")
 ]
